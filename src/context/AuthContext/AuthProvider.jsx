19d2267d598d44b05a081e8377f8353e8a958a98
Arman <mdarman365506@gmail.com>
2024-12-16 21:18:22 +0600

    added token add and remove option using axios

--- src/context/AuthContext/AuthProvider.jsx
+++ src/context/AuthContext/AuthProvider.jsx
@@ -2,6 +2,7 @@ import React, { useEffect, useState } from 'react';
 import AuthContext from './AuthContext';
 import { createUserWithEmailAndPassword, GoogleAuthProvider, onAuthStateChanged, signInWithEmailAndPassword, signInWithPopup, signOut } from "firebase/auth";
 import auth from '../../firebase/firebase.init';
+import axios from 'axios';
 
 const googleProvider = new GoogleAuthProvider();
 
@@ -33,7 +34,21 @@ const AuthProvider = ({ children }) => {
     useEffect(() => {
         const unsubscribe = onAuthStateChanged(auth, currentUser => {
             setUser(currentUser);
-            console.log('state captured', currentUser)
+            console.log('state captured', currentUser?.email)
+            if (currentUser?.email) {
+                const user = { email: currentUser.email };
+
+                axios.post("http://localhost:5000/jwt", user, {withCredentials:true})
+                    .then(res => console.log('log in',res.data));
+            }
+            else {
+                axios.post("http://localhost:5000/logout", {}, {
+                    withCredentials: true
+                })
+                    .then(res => console.log('logout', res.data));
+            }
+                
+                //put it in the right place
             setLoading(false);
         })
 
